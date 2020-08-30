4d50f291311897a0ca08a221559311b7396c93d4
yongtany <yongn1494@gmail.com>
2020-08-30 17:05:50 +0900

    add: Error Handling

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,23 +1,16 @@
 import {
   Entity,
   Column,
-  PrimaryColumn,
-  BeforeInsert,
-  BaseEntity
+  BaseEntity,
+  PrimaryGeneratedColumn
 } from "typeorm";
-import * as uuidv4 from "uuid/v4";
 
 @Entity("users")
 export class User extends BaseEntity {
-  @PrimaryColumn("uuid") id: string;
+  @PrimaryGeneratedColumn("uuid") id: string;
 
   @Column("varchar", { length: 255 })
   email: string;
 
   @Column("text") password: string;
-
-  @BeforeInsert()
-  addId() {
-    this.id = uuidv4();
-  }
 }
--- src/modules/register/resgister.test.ts
+++ src/modules/register/resgister.test.ts
@@ -17,16 +17,22 @@ const password = "test123";
 
 const mutation = `
   mutation {
-    register(email: "${email}", password: "${password}")
+    register(email: "${email}", password: "${password}") {
+      path
+      message
+    }
   }
 `;
 
-test('Register user', async () => {
+test("Register user", async () => {
   const response = await request(getHost(), mutation);
-  expect(response).toEqual({ register: true });
-  const users = await User.find({ where: { email }});
+  expect(response).toEqual({ register: null });
+  const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
   const user = users[0];
   expect(user.email).toEqual(email);
   expect(user.password).not.toEqual(password);
+  const response2: any = await request(getHost(), mutation);
+  expect(response2.register).toHaveLength(1);
+  expect(response2.register[0].path).toEqual("email");
 });
