67ebc7675d5c391c7a59b61bd4b93b1d2f5656b3
yongtany <yongn1494@gmail.com>
2020-09-03 20:55:53 +0900

    add: login mutation

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,6 +1,5 @@
 import * as bcrypt from "bcryptjs";
 import * as yup from "yup";
-import { v4 } from "uuid";
 
 import { ResolverMap } from "../../types/graphql-utils";
 import { User } from "../../entity/User";
@@ -11,8 +10,8 @@ import {
   invalidEmail,
   passwordNotLongEnough
 } from "./errorMessages";
-import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
-import { sendEmail } from "../../utils/sendEmail";
+// import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
+// import { sendEmail } from "../../utils/sendEmail";
 
 const schema = yup.object().shape({
   email: yup
@@ -34,7 +33,7 @@ export const resolvers: ResolverMap = {
     register: async (
       _,
       args: GQL.IRegisterOnMutationArguments,
-      { redis, url }
+      // { redis, url }
     ) => {
       try {
         await schema.validate(args, { abortEarly: false });
@@ -60,19 +59,18 @@ export const resolvers: ResolverMap = {
 
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
-        id: v4(),
         email,
         password: hashedPassword
       });
 
       await user.save();
 
-      if (process.env.NODE_ENV !== "test") {
-        await sendEmail(
-          email,
-          await createConfirmEmailLink(url, user.id, redis)
-        );
-      }
+      // if (process.env.NODE_ENV !== "test") {
+      //   await sendEmail(
+      //     email,
+      //     await createConfirmEmailLink(url, user.id, redis)
+      //   );
+      // }
 
       return null;
     }
--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -2,44 +2,56 @@
 // graphql typescript definitions
 
 declare namespace GQL {
-interface IGraphQLResponseRoot {
-data?: IQuery | IMutation;
-errors?: Array<IGraphQLResponseError>;
-}
-
-interface IGraphQLResponseError {
-/** Required for all errors */
-message: string;
-locations?: Array<IGraphQLResponseErrorLocation>;
-/** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
-[propName: string]: any;
-}
-
-interface IGraphQLResponseErrorLocation {
-line: number;
-column: number;
-}
-
-interface IQuery {
-__typename: "Query";
-bye: string | null;
-}
-
-interface IMutation {
-__typename: "Mutation";
-register: Array<IError> | null;
-}
-
-interface IRegisterOnMutationArguments {
-email: string;
-password: string;
-}
-
-interface IError {
-__typename: "Error";
-path: string;
-message: string;
-}
-}
-
-// tslint:enable
+  interface IGraphQLResponseRoot {
+  data?: IQuery | IMutation;
+  errors?: Array<IGraphQLResponseError>;
+  }
+  
+  interface IGraphQLResponseError {
+  /** Required for all errors */
+  message: string;
+  locations?: Array<IGraphQLResponseErrorLocation>;
+  /** 7.2.2 says 'GraphQL servers may provide additional entries to error' */
+  [propName: string]: any;
+  }
+  
+  interface IGraphQLResponseErrorLocation {
+  line: number;
+  column: number;
+  }
+  
+  interface IQuery {
+  __typename: "Query";
+  bye2: string | null;
+  bye: string | null;
+  hello: string;
+  }
+  
+  interface IHelloOnQueryArguments {
+  name?: string | null;
+  }
+  
+  interface IMutation {
+  __typename: "Mutation";
+  login: Array<IError>;
+  register: Array<IError>;
+  }
+  
+  interface ILoginOnMutationArguments {
+  email: string;
+  password: string;
+  }
+  
+  interface IRegisterOnMutationArguments {
+  email: string;
+  password: string;
+  }
+  
+  interface IError {
+  __typename: "Error";
+  path: string;
+  message: string;
+  }
+  }
+  
+  // tslint:enable
